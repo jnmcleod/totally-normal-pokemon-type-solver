7a376904b47b58d970a8c3e2e9aa84a0489432d5
jnmcleod <113480362+jnmcleod@users.noreply.github.com>
2022-09-17 14:30:04 -0600

    Initial code upload

--- totally-normal-pokemon-type-solver/main.cpp
+++ totally-normal-pokemon-type-solver/main.cpp
@@ -1,14 +1,65 @@
-//
-//  main.cpp
-//  totally-normal-pokemon-type-solver
-//
-//  Created by Jesse McLeod on 2022-09-17.
-//
-
+#include <fstream>
 #include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+extern char** environ;
+
+void createFiles()
+{
+    std::ifstream inputFile("sourcecode");
+    if (!inputFile)
+    {
+        std::cout << "Source file not found\n";
+        exit(-1);
+    }
+    
+    std::ofstream outputFile;
+    std::string temp;
+    
+    while (getline(inputFile, temp))
+    {
+        if (temp[0] == '/' && temp[1] == '/')
+        {
+            outputFile.close();
+            outputFile.open(&temp[2], std::ofstream::app);
+        }
+        else
+        {
+            outputFile << temp << std::endl;
+        }
+    }
+    
+    inputFile.close();
+    outputFile.close();
+}
+
+void compileFiles()
+{
+    pid_t child = fork();
+    if (child == 0)
+    {
+        int fd = open("log.txt", O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+        dup2(fd, STDERR_FILENO);
+        execlp("javac", "javac", "Javacode.java", NULL);
+    }
+    else
+    {
+        child = fork();
+        if (child == 0)
+        {
+            execlp("g++", "g++", "-o", "cppcode", "cppcode.cpp", NULL);
+        }
+    }
+    
+}
+
+int main()
+{
+    createFiles();
+    compileFiles();
+    
     return 0;
 }
