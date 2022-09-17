e8b650c848699820f8760b9a845c4c4e93138a9c
jnmcleod <113480362+jnmcleod@users.noreply.github.com>
2022-09-17 14:12:32 -0600

    Initial Commit

---
+++ totally-normal-pokemon-type-solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  totally-normal-pokemon-type-solver
+//
+//  Created by Jesse McLeod on 2022-09-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
